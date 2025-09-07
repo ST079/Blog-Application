1750a36da5e32453e6af3dad85b361f021754495
Sujan Tamang <suzanyba079@gmail.com>
2025-09-07 14:21:41 +0545

    navigation bar

--- client/src/layouts/NavigationBar.jsx
+++ client/src/layouts/NavigationBar.jsx
@@ -1,69 +1,204 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const NavigationBar = () => {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+
   return (
     <div class="container">
-      {" "}
-      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        {" "}
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
         <div class="col-md-3 mb-2 mb-md-0">
-          {" "}
           <a
             href="/"
             class="d-inline-flex link-body-emphasis text-decoration-none"
           >
-            {" "}
-            <svg
-              class="bi"
-              width="40"
-              height="32"
-              role="img"
-              aria-label="Bootstrap"
-            >
-              <use xlink:href="#bootstrap"></use>
-            </svg>{" "}
-          </a>{" "}
-        </div>{" "}
+            <img src="/logo.png" alt="" height={30} width={30} />
+          </a>
+        </div>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          {" "}
           <li>
             <a href="#" class="nav-link px-2 link-secondary">
               Home
             </a>
-          </li>{" "}
-          <li>
-            <a href="#" class="nav-link px-2">
-              Features
-            </a>
-          </li>{" "}
-          <li>
-            <a href="#" class="nav-link px-2">
-              Pricing
-            </a>
-          </li>{" "}
-          <li>
-            <a href="#" class="nav-link px-2">
-              FAQs
-            </a>
-          </li>{" "}
+          </li>
           <li>
             <a href="#" class="nav-link px-2">
               About
             </a>
-          </li>{" "}
-        </ul>{" "}
+          </li>
+          <li>
+            <a href="#" class="nav-link px-2">
+              Blogs
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link px-2">
+              FAQs
+            </a>
+          </li>
+        </ul>
         <div class="col-md-3 text-end">
-          {" "}
-          <button type="button" class="btn btn-outline-primary me-2">
+          <button
+            type="button"
+            class="btn btn-outline-primary me-2"
+            onClick={() => setShowLoginModal(true)}
+          >
             Login
-          </button>{" "}
-          <button type="button" class="btn btn-primary">
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={() => setShowSignupModal(true)}
+          >
             Sign-up
-          </button>{" "}
-        </div>{" "}
-      </header>{" "}
+          </button>
+        </div>
+      </header>
+
+      {/* Login Modal */}
+      {showLoginModal && (
+        <div className="modal fade show d-block" tabIndex="-1">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Login to Your Account</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setShowLoginModal(false)}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">
+                      Email address
+                    </label>
+                    <input type="email" className="form-control" id="email" />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                    />
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="remember"
+                    />
+                    <label className="form-check-label" htmlFor="remember">
+                      Remember me
+                    </label>
+                  </div>
+                  <button type="submit" className="btn btn-primary w-100">
+                    Login
+                  </button>
+                </form>
+                <div className="text-center mt-3">
+                  <a href="#" className="text-decoration-none">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Signup Modal */}
+      {showSignupModal && (
+        <div className="modal fade show d-block" tabIndex="-1">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Create an Account</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setShowSignupModal(false)}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="row mb-3">
+                    <div className="col">
+                      <label htmlFor="firstName" className="form-label">
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="firstName"
+                      />
+                    </div>
+                    <div className="col">
+                      <label htmlFor="lastName" className="form-label">
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="lastName"
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="signupEmail" className="form-label">
+                      Email address
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="signupEmail"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="signupPassword" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="signupPassword"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="confirmPassword" className="form-label">
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="confirmPassword"
+                    />
+                  </div>
+                  <div className="mb-3 form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="terms"
+                    />
+                    <label className="form-check-label" htmlFor="terms">
+                      I agree to the Terms and Conditions
+                    </label>
+                  </div>
+                  <button type="submit" className="btn btn-primary w-100">
+                    Sign Up
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default NavigationBar
+export default NavigationBar;
--- client/src/pages/User/CollegeBlogs.jsx
+++ client/src/pages/User/CollegeBlogs.jsx
@@ -4,8 +4,6 @@ import "../../styles/CollegeBlogs.css";
 
 const CollegeBlogs = () => {
   const [activeTab, setActiveTab] = useState("all");
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const [showSignupModal, setShowSignupModal] = useState(false);
 
   // Sample blog posts data
   const blogPosts = [
@@ -71,9 +69,6 @@ const CollegeBlogs = () => {
 
   return (
     <div className="college-blog">
-      {/* Navigation Bar */}
-      
-
       {/* Hero Section */}
       <section className="hero-section">
         <div className="container">
@@ -452,149 +447,6 @@ const CollegeBlogs = () => {
           </div>
         </div>
       </footer>
-
-      {/* Login Modal */}
-      {showLoginModal && (
-        <div className="modal fade show d-block" tabIndex="-1">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Login to Your Account</h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  onClick={() => setShowLoginModal(false)}
-                ></button>
-              </div>
-              <div className="modal-body">
-                <form>
-                  <div className="mb-3">
-                    <label htmlFor="email" className="form-label">
-                      Email address
-                    </label>
-                    <input type="email" className="form-control" id="email" />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                    />
-                  </div>
-                  <div className="mb-3 form-check">
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      id="remember"
-                    />
-                    <label className="form-check-label" htmlFor="remember">
-                      Remember me
-                    </label>
-                  </div>
-                  <button type="submit" className="btn btn-primary w-100">
-                    Login
-                  </button>
-                </form>
-                <div className="text-center mt-3">
-                  <a href="#" className="text-decoration-none">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* Signup Modal */}
-      {showSignupModal && (
-        <div className="modal fade show d-block" tabIndex="-1">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Create an Account</h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  onClick={() => setShowSignupModal(false)}
-                ></button>
-              </div>
-              <div className="modal-body">
-                <form>
-                  <div className="row mb-3">
-                    <div className="col">
-                      <label htmlFor="firstName" className="form-label">
-                        First Name
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="firstName"
-                      />
-                    </div>
-                    <div className="col">
-                      <label htmlFor="lastName" className="form-label">
-                        Last Name
-                      </label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="lastName"
-                      />
-                    </div>
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="signupEmail" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="signupEmail"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="signupPassword" className="form-label">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="signupPassword"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="confirmPassword" className="form-label">
-                      Confirm Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="confirmPassword"
-                    />
-                  </div>
-                  <div className="mb-3 form-check">
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      id="terms"
-                    />
-                    <label className="form-check-label" htmlFor="terms">
-                      I agree to the Terms and Conditions
-                    </label>
-                  </div>
-                  <button type="submit" className="btn btn-primary w-100">
-                    Sign Up
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
